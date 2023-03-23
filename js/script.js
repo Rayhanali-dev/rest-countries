c8d81f48dceb4b4b95afb6191d3c22273be36991
Rayhanali-dev <rayhanalidev@gmail.com>
2023-03-23 23:50:58 +0600

    see more and see less funcionality completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+let showData = 9;
+let allData;
 const loadCountry = () => {
     fetch(`https://restcountries.com/v3.1/all`)
     .then(res => res.json())
-    .then(data => displayData(data.slice(0, 6)))
+    .then(data => displayData(data))
 }
 
 const displayData = (countries) => {
     const allCountriesHTML = countries.map(country => getCountry(country))
     const container = document.getElementById('countries');
-    container.innerHTML = allCountriesHTML.join(' ');
+    container.innerHTML = allCountriesHTML.slice(0, showData).join(' ');
+    allData = allCountriesHTML;
 }
 // destructuring 
 const getCountry = ({name, flags, area, region}) => {
@@ -30,17 +33,28 @@ const getCountry = ({name, flags, area, region}) => {
 //     `;
 // }
 
-
-const loadCountry2 = () => {
-    fetch(`https://restcountries.com/v3.1/all`)
-    .then(res => res.json())
-    .then(data => displayData(data))
-}
 const hideBtn = document.getElementById('hide-btn');
+const previous = document.getElementsByClassName('show-less')[0];
 document.getElementById('show-all-btn').addEventListener('click', function(){
-    loadCountry2()
-    hideBtn.style.display = 'none';
+
+    showData += 9;
+    loadCountry();
+    if (showData >= allData.length) {
+        hideBtn.style.display = 'none';
+        previous.style.display = 'block';
+    }
 })
+
+document.getElementById('previous').onclick = () => {
+    showData -= 9;
+
+    if (showData <= 9) {
+        showData = 9;
+        hideBtn.style.display = 'block';
+        previous.style.display = 'none';
+    }
+    loadCountry();
+}
 
 loadCountry()
 
